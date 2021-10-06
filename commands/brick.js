023cd0145ee34cc16f93a339db4f11b85d52cddd
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2021-10-06 01:30:34 -0400

    Automated brick gif implementation
    Now I don't need to run deploy-commands to add gifs :)
    Now I don't need to add to a switch case to add gifs :)
    Now I don't need to restart the bot to a switch case to add gifs :)

--- commands/brick.js
+++ commands/brick.js
@@ -1,4 +1,5 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
+const fs = require('fs');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,33 +12,10 @@ module.exports = {
         ),
     async execute(interaction) {
         const user = interaction.options.getUser('user')
-        let num = Math.floor(Math.random() * 7) + 1
-        let file
-        switch (num) {
-            case (1):
-                file = "./assets/brick1.gif"
-                break
-            case (2):
-                file = "./assets/brick2.gif"
-                break
-            case (3):
-                file = "./assets/brick3.gif"
-                break
-            case (4):
-                file = "./assets/brick4.gif"
-                break
-            case (5):
-                file = "./assets/brick5.gif"
-                break
-            case (6):
-                file = "./assets/brick6.gif"
-                break
-            case (7):
-                file = "./assets/brick7.gif"
-                break
-            default:
-                file = "./assets/brick1.gif"
-        }
+        let brickFiles = fs.readdirSync('./assets').filter(file => file.startsWith('brick'));
+        
+        let file = 'assets\\\\' + brickFiles[Math.floor(Math.random() * brickFiles.length)]
+
         return await interaction.reply({ content: `${user} has been bricked.`, files: [file] });
     },
 };
