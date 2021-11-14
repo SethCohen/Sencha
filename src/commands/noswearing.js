b9d1c23578d47c7c53202de49fe2236596aaaee7
SethCohen <sethcohen.dev@gmail.com>
2021-11-14 15:18:14 -0500

    Changed `/noswearing` into `/servercopypasta` and added more versatility to command.

--- src/commands/noswearing.js
+++ src/commands/noswearing.js
@@ -1,15 +1,18 @@
-const {SlashCommandBuilder} = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('noswearing')
-        .setDescription('Reminds people what our servers virtues are about.')
-        .addUserOption(option =>
-        option.setName('user')
-            .setDescription('The user to remind not to swear.')),
+	data: new SlashCommandBuilder()
+		.setName('servercopypasta')
+		.setDescription('Reminds people what our servers virtues are about.')
+		.addUserOption(option =>
+			option.setName('user')
+				.setDescription('The user to mention.'))
+		.addStringOption(option =>
+			option.setName('prefix').setDescription('Prefix message to put in front of the copypasta. e.g. "No Swearing in"')),
 
-    async execute(interaction) {
-        const user = interaction.options.getUser('user')
-        return await interaction.reply({content: `${user ? user : ''} No swearing in our good christian atheist muslim jewish buddhist hindu taoist shinto sikh various aboriginal religions wicca pagan orange juice pastafarianism tea club server!`, files: ['./assets/noswearing.jpg']})
-    },
+	async execute(interaction) {
+		const user = interaction.options.getUser('user');
+		const prefix = interaction.options.getString('prefix');
+		return await interaction.reply({ content: `${user ? user : ''} ${prefix ? prefix : ''} our good christian atheist muslim jewish buddhist hindu taoist shinto sikh various aboriginal religions wicca pagan orange juice pastafarianism tea club server!`, files: ['./src/assets/threaten.jpg'] });
+	},
 };
